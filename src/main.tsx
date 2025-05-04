c6b726ad3d20aa81a3249194864b3df99c659ed0
crmjkuniversitario <crmjkuniversitario@gmail.com>
2025-05-04 23:50:46 +0000

    feat: adiciona página de cadastro no App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import './index.css'; // se estiver usando Tailwind ou CSS global
+//import './index.css'; // Opcional, se estiver usando estilos globais
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
