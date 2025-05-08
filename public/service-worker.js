cbb1074b8391f77b84ecb00a4a9c88f319d3e1d7
crmjkuniversitario <crmjkuniversitario@gmail.com>
2025-05-07 22:01:22 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,11 @@
+// service-worker.js
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  // Aqui você pode adicionar cache de arquivos se necessário
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker ativado');
+});
 
